22d1c51a0fc3471a0401e4d6604803b4a95cd2f5
niklr <r.niklaus@gmx.ch>
2021-05-09 23:06:26 +0200

    feat: extend GraphQL schema with StatsContainer, Transaction and TransferEvent entities

--- subgraph-snapshot/generated/schema.ts
+++ subgraph-snapshot/generated/schema.ts
@@ -42,6 +42,15 @@ export class Account extends Entity {
     this.set("id", Value.fromString(value));
   }
 
+  get index(): BigInt {
+    let value = this.get("index");
+    return value.toBigInt();
+  }
+
+  set index(value: BigInt) {
+    this.set("index", Value.fromBigInt(value));
+  }
+
   get totalSupply(): boolean {
     let value = this.get("totalSupply");
     return value.toBoolean();
@@ -127,6 +136,15 @@ export class AccountSnapshot extends Entity {
     this.set("id", Value.fromString(value));
   }
 
+  get index(): BigInt {
+    let value = this.get("index");
+    return value.toBigInt();
+  }
+
+  set index(value: BigInt) {
+    this.set("index", Value.fromBigInt(value));
+  }
+
   get account(): string {
     let value = this.get("account");
     return value.toString();
@@ -179,5 +197,277 @@ export class AccountSnapshot extends Entity {
 
   set blockTimestamp(value: BigInt) {
     this.set("blockTimestamp", Value.fromBigInt(value));
+  }
+}
+
+export class StatsContainer extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save StatsContainer entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save StatsContainer entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("StatsContainer", id.toString(), this);
+  }
+
+  static load(id: string): StatsContainer | null {
+    return store.get("StatsContainer", id) as StatsContainer | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get lastAccountIndex(): BigInt {
+    let value = this.get("lastAccountIndex");
+    return value.toBigInt();
+  }
+
+  set lastAccountIndex(value: BigInt) {
+    this.set("lastAccountIndex", Value.fromBigInt(value));
+  }
+
+  get lastAccountSnapshotIndex(): BigInt {
+    let value = this.get("lastAccountSnapshotIndex");
+    return value.toBigInt();
+  }
+
+  set lastAccountSnapshotIndex(value: BigInt) {
+    this.set("lastAccountSnapshotIndex", Value.fromBigInt(value));
+  }
+
+  get lastTransactionIndex(): BigInt {
+    let value = this.get("lastTransactionIndex");
+    return value.toBigInt();
+  }
+
+  set lastTransactionIndex(value: BigInt) {
+    this.set("lastTransactionIndex", Value.fromBigInt(value));
+  }
+
+  get lastTransferEventIndex(): BigInt {
+    let value = this.get("lastTransferEventIndex");
+    return value.toBigInt();
+  }
+
+  set lastTransferEventIndex(value: BigInt) {
+    this.set("lastTransferEventIndex", Value.fromBigInt(value));
+  }
+}
+
+export class Transaction extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Transaction entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Transaction entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Transaction", id.toString(), this);
+  }
+
+  static load(id: string): Transaction | null {
+    return store.get("Transaction", id) as Transaction | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get index(): BigInt {
+    let value = this.get("index");
+    return value.toBigInt();
+  }
+
+  set index(value: BigInt) {
+    this.set("index", Value.fromBigInt(value));
+  }
+
+  get from(): Bytes {
+    let value = this.get("from");
+    return value.toBytes();
+  }
+
+  set from(value: Bytes) {
+    this.set("from", Value.fromBytes(value));
+  }
+
+  get to(): Bytes | null {
+    let value = this.get("to");
+    if (value === null || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set to(value: Bytes | null) {
+    if (value === null) {
+      this.unset("to");
+    } else {
+      this.set("to", Value.fromBytes(value as Bytes));
+    }
+  }
+
+  get blockNumber(): BigInt {
+    let value = this.get("blockNumber");
+    return value.toBigInt();
+  }
+
+  set blockNumber(value: BigInt) {
+    this.set("blockNumber", Value.fromBigInt(value));
+  }
+
+  get blockTimestamp(): BigInt {
+    let value = this.get("blockTimestamp");
+    return value.toBigInt();
+  }
+
+  set blockTimestamp(value: BigInt) {
+    this.set("blockTimestamp", Value.fromBigInt(value));
+  }
+
+  get transferEvents(): Array<string> {
+    let value = this.get("transferEvents");
+    return value.toStringArray();
+  }
+
+  set transferEvents(value: Array<string>) {
+    this.set("transferEvents", Value.fromStringArray(value));
+  }
+}
+
+export class TransferEvent extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save TransferEvent entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save TransferEvent entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("TransferEvent", id.toString(), this);
+  }
+
+  static load(id: string): TransferEvent | null {
+    return store.get("TransferEvent", id) as TransferEvent | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get index(): BigInt {
+    let value = this.get("index");
+    return value.toBigInt();
+  }
+
+  set index(value: BigInt) {
+    this.set("index", Value.fromBigInt(value));
+  }
+
+  get transaction(): string {
+    let value = this.get("transaction");
+    return value.toString();
+  }
+
+  set transaction(value: string) {
+    this.set("transaction", Value.fromString(value));
+  }
+
+  get from(): Bytes {
+    let value = this.get("from");
+    return value.toBytes();
+  }
+
+  set from(value: Bytes) {
+    this.set("from", Value.fromBytes(value));
+  }
+
+  get to(): Bytes {
+    let value = this.get("to");
+    return value.toBytes();
+  }
+
+  set to(value: Bytes) {
+    this.set("to", Value.fromBytes(value));
+  }
+
+  get amount(): BigDecimal {
+    let value = this.get("amount");
+    return value.toBigDecimal();
+  }
+
+  set amount(value: BigDecimal) {
+    this.set("amount", Value.fromBigDecimal(value));
+  }
+
+  get logIndex(): BigInt {
+    let value = this.get("logIndex");
+    return value.toBigInt();
+  }
+
+  set logIndex(value: BigInt) {
+    this.set("logIndex", Value.fromBigInt(value));
+  }
+
+  get blockNumber(): BigInt {
+    let value = this.get("blockNumber");
+    return value.toBigInt();
+  }
+
+  set blockNumber(value: BigInt) {
+    this.set("blockNumber", Value.fromBigInt(value));
+  }
+
+  get blockTimestamp(): BigInt {
+    let value = this.get("blockTimestamp");
+    return value.toBigInt();
+  }
+
+  set blockTimestamp(value: BigInt) {
+    this.set("blockTimestamp", Value.fromBigInt(value));
+  }
+
+  get tokenType(): string {
+    let value = this.get("tokenType");
+    return value.toString();
+  }
+
+  set tokenType(value: string) {
+    this.set("tokenType", Value.fromString(value));
   }
 }
--- subgraph-snapshot/src/mapping.ts
+++ subgraph-snapshot/src/mapping.ts
@@ -1,56 +1,65 @@
-import { Transfer } from '../generated/xHOPR/xHOPR'
+import { Address, BigDecimal, BigInt, ethereum } from '@graphprotocol/graph-ts'
+import { Account, AccountSnapshot, StatsContainer, Transaction, TransferEvent } from '../generated/schema'
 import { Transfer as wxTransfer } from '../generated/wxHOPR/wxHOPR'
-import { Account, AccountSnapshot } from '../generated/schema'
-import { Address, BigDecimal, BigInt, } from '@graphprotocol/graph-ts'
+import { Transfer as xTransfer } from '../generated/xHOPR/xHOPR'
 import { convertEthToDecimal, zeroBD, zeroBigInt } from './helpers'
 
-export function handleXHoprTransfer(event: Transfer ): void {
+export function handleXHoprTransfer(event: xTransfer): void {
   // xHOPR has 18 decimals
   const value = convertEthToDecimal(event.params.value);
-  const blockNumber = event.block.number;
-  const blockTimestamp = event.block.timestamp;
-
-  handleAccountSnapshotUpdate(event.params.from, value, blockNumber, blockTimestamp, true, true);
-  handleAccountSnapshotUpdate(event.params.to, value, blockNumber, blockTimestamp, true, false);
+  handleTransfer(event, event.params.from, event.params.to, value, TokenType.xHOPR);
 }
 
 export function handleWxHoprTransfer(event: wxTransfer): void {
   // wxHOPR has 18 decimals
   const value = convertEthToDecimal(event.params.value);
+  handleTransfer(event, event.params.from, event.params.to, value, TokenType.wxHOPR);
+}
+
+enum TokenType {
+  xHOPR = 0,
+  wxHOPR = 1
+}
+
+function handleTransfer(event: ethereum.Event, from: Address, to: Address, value: BigDecimal, type: TokenType): void {
   const blockNumber = event.block.number;
   const blockTimestamp = event.block.timestamp;
 
-  handleAccountSnapshotUpdate(event.params.from, value, blockNumber, blockTimestamp, false, true);
-  handleAccountSnapshotUpdate(event.params.to, value, blockNumber, blockTimestamp, false, false);
+  // Keep track of all entity indexes which are needed for pagination purposes
+  // https://thegraph.com/docs/graphql-api#pagination
+  const stats = getOrCreateStatsContainerEntity();
+
+  handleAccountSnapshotUpdate(from, value, blockNumber, blockTimestamp, type, true, stats);
+  handleAccountSnapshotUpdate(to, value, blockNumber, blockTimestamp, type, false, stats);
+
+  getOrCreateTransactionEntity(event.transaction, blockNumber, blockTimestamp, stats);
+  getOrCreateTransferEventEntity(event, from, to, value, blockNumber, blockTimestamp, type, stats);
+
+  stats.save();
 }
 
-function handleAccountSnapshotUpdate(address: Address, value: BigDecimal, block: BigInt, timestamp: BigInt, isXHopr: boolean, isFrom: boolean): void {
+function handleAccountSnapshotUpdate(address: Address, value: BigDecimal, block: BigInt, timestamp: BigInt, type: TokenType, isFrom: boolean, stats: StatsContainer): void {
   const accountId = address.toHexString();
-  let account = Account.load(accountId);
-  if (account == null) {
-    createAccountEntity(accountId);
-    account = Account.load(accountId); // reload here
-  }
+  const account = getOrCreateAccountEntity(accountId, stats);
 
   const snapshotId = accountId + "-" + block.toString();
-  const isAdd = account.totalSupply ? isFrom : !isFrom;     // id of address(0) represents the total supply. 
+  const isAdd = account.totalSupply ? isFrom : !isFrom; // id of address(0) represents the total supply. 
 
-  let snapshot = AccountSnapshot.load(snapshotId);
-  if (snapshot == null) {
-    createAccountSnapshotEntity(accountId, snapshotId)
-    snapshot = AccountSnapshot.load(snapshotId); // reload here
-  }
+  const snapshot = getOrCreateAccountSnapshotEntity(accountId, snapshotId, stats);
 
   // handleAccountUpdate
-  if (isXHopr) {
-    const newXHoprBalance = isAdd ? account.xHoprBalance.plus(value) : account.xHoprBalance.minus(value);
-    account.xHoprBalance = newXHoprBalance;
-    snapshot.xHoprBalance = newXHoprBalance;
-
-  } else {
-    const newWxHoprBalance = isAdd ? account.wxHoprBalance.plus(value) : account.wxHoprBalance.minus(value);
-    account.wxHoprBalance = newWxHoprBalance;
-    snapshot.wxHoprBalance = newWxHoprBalance;
+  switch (type) {
+    case TokenType.xHOPR:
+      const newXHoprBalance = isAdd ? account.xHoprBalance.plus(value) : account.xHoprBalance.minus(value);
+      account.xHoprBalance = newXHoprBalance;
+      snapshot.xHoprBalance = newXHoprBalance;
+      break;
+    case TokenType.wxHOPR:
+    default:
+      const newWxHoprBalance = isAdd ? account.wxHoprBalance.plus(value) : account.wxHoprBalance.minus(value);
+      account.wxHoprBalance = newWxHoprBalance;
+      snapshot.wxHoprBalance = newWxHoprBalance;
+      break;
   }
 
   const newTotalBalance = isAdd ? account.totalBalance.plus(value) : account.totalBalance.minus(value);
@@ -66,24 +75,100 @@ function handleAccountSnapshotUpdate(address: Address, value: BigDecimal, block:
   snapshot.save()
 }
 
-function createAccountEntity(accountId: string): void {
-  const account = new Account(accountId);
-  account.totalSupply = accountId == "0x0000000000000000000000000000000000000000";
-  account.xHoprBalance = zeroBD();
-  account.wxHoprBalance = zeroBD();
-  account.totalBalance = zeroBD();
-  account.blockNumber = zeroBigInt();
-  account.blockTimestamp = zeroBigInt();
-  account.save()
+function getOrCreateStatsContainerEntity(): StatsContainer {
+  const entityId = '1';
+  let entity = StatsContainer.load(entityId);
+  if (entity == null) {
+    entity = new StatsContainer(entityId);
+    entity.lastAccountIndex = zeroBigInt();
+    entity.lastAccountSnapshotIndex = zeroBigInt();
+    entity.lastTransactionIndex = zeroBigInt();
+    entity.lastTransferEventIndex = zeroBigInt();
+    entity.save();
+  }
+  return entity as StatsContainer;
 }
 
-function createAccountSnapshotEntity(accountId: string, snapshotId: string): void {
-  const snapshot = new AccountSnapshot(snapshotId);
-  snapshot.account = accountId;
-  snapshot.xHoprBalance = zeroBD();
-  snapshot.wxHoprBalance = zeroBD();
-  snapshot.totalBalance = zeroBD();
-  snapshot.blockNumber = zeroBigInt();
-  snapshot.blockTimestamp = zeroBigInt();
-  snapshot.save()
+function getOrCreateAccountEntity(id: string, stats: StatsContainer): Account {
+  let entity = Account.load(id);
+  if (entity == null) {
+    entity = new Account(id);
+    entity.index = stats.lastAccountIndex.plus(BigInt.fromI32(1));
+    entity.totalSupply = id == "0x0000000000000000000000000000000000000000";
+    entity.xHoprBalance = zeroBD();
+    entity.wxHoprBalance = zeroBD();
+    entity.totalBalance = zeroBD();
+    entity.blockNumber = zeroBigInt();
+    entity.blockTimestamp = zeroBigInt();
+    entity.save();
+    stats.lastAccountIndex = entity.index;
+  }
+  return entity as Account;
+}
+
+function getOrCreateAccountSnapshotEntity(accountId: string, snapshotId: string, stats: StatsContainer): AccountSnapshot {
+  let entity = AccountSnapshot.load(snapshotId);
+  if (entity == null) {
+    entity = new AccountSnapshot(snapshotId);
+    entity.index = stats.lastAccountSnapshotIndex.plus(BigInt.fromI32(1));
+    entity.account = accountId;
+    entity.xHoprBalance = zeroBD();
+    entity.wxHoprBalance = zeroBD();
+    entity.totalBalance = zeroBD();
+    entity.blockNumber = zeroBigInt();
+    entity.blockTimestamp = zeroBigInt();
+    entity.save()
+    stats.lastAccountSnapshotIndex = entity.index;
+  }
+  return entity as AccountSnapshot;
+}
+
+function getOrCreateTransactionEntity(transaction: ethereum.Transaction, block: BigInt, timestamp: BigInt, stats: StatsContainer): Transaction {
+  const id = getTransactionId(transaction);
+  let entity = Transaction.load(id);
+  if (entity == null) {
+    entity = new Transaction(id);
+    entity.index = stats.lastTransactionIndex.plus(BigInt.fromI32(1));
+    entity.from = transaction.from;
+    entity.to = transaction.to;
+    entity.blockNumber = block;
+    entity.blockTimestamp = timestamp;
+    entity.save();
+    stats.lastTransactionIndex = entity.index;
+  }
+  return entity as Transaction;
+}
+
+function getOrCreateTransferEventEntity(
+  event: ethereum.Event, from: Address, to: Address, value: BigDecimal, block: BigInt, timestamp: BigInt, type: TokenType, stats: StatsContainer
+): TransferEvent {
+  const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  const entity = new TransferEvent(id);
+  entity.index = stats.lastTransferEventIndex.plus(BigInt.fromI32(1));
+  entity.transaction = getTransactionId(event.transaction);
+  entity.from = from;
+  entity.to = to;
+  entity.amount = value;
+  entity.logIndex = event.logIndex;
+  entity.blockNumber = block;
+  entity.blockTimestamp = timestamp;
+  entity.tokenType = tokenTypeToString(type);
+  entity.save();
+  stats.lastTransferEventIndex = entity.index;
+  return entity;
+}
+
+function getTransactionId(transaction: ethereum.Transaction): string {
+  return transaction.hash.toHexString();
+}
+
+function tokenTypeToString(type: TokenType): string {
+  switch (type) {
+    case TokenType.wxHOPR:
+      return 'wxHOPR';
+    case TokenType.xHOPR:
+      return 'xHOPR';
+    default:
+      return 'Unknown';
+  }
 }
