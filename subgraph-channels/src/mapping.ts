5840eb8918846ebfb5b3f4ef374c271e44952fb1
jjperezaguinaga <me@jjperezaguinaga.com>
2021-07-02 15:12:36 +0000

    feat: Adding subgraph tooling

--- subgraph-channels/src/mapping.ts
+++ subgraph-channels/src/mapping.ts
@@ -1,8 +1,5 @@
-/*
-  TODO: add mappings
+import { log } from '@graphprotocol/graph-ts'
+import { Announcement, ChannelUpdate } from '../generated/HoprChannels/HoprChannels'
 
-  You can view the HoprChannels smart contract at https://github.com/hoprnet/hoprnet/blob/828e5b3935ff6da74dd512cc0972d4a31339af29/packages/ethereum/contracts/HoprChannels.sol
-  We want to track these events and reduce our data from them.
-  - announce: https://github.com/hoprnet/hoprnet/blob/828e5b3935ff6da74dd512cc0972d4a31339af29/packages/ethereum/contracts/HoprChannels.sol#L70
-  - channel update: https://github.com/hoprnet/hoprnet/blob/828e5b3935ff6da74dd512cc0972d4a31339af29/packages/ethereum/contracts/HoprChannels.sol#L75
-*/
+export function handleAnnouncement(event: Announcement) {}
+export function handleChannelUpdate(event: ChannelUpdate) {}
