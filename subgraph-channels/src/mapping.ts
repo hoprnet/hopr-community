4b08e4dad843c7ac7fa5d855195c5ec683b4845c
jjperezaguinaga <me@jjperezaguinaga.com>
2021-07-09 03:54:24 +0000

    feat: Actually add announce mappings

--- subgraph-channels/src/mapping.ts
+++ subgraph-channels/src/mapping.ts
@@ -3,8 +3,14 @@ import { Announcement, ChannelUpdate } from '../generated/HoprChannels/HoprChann
 import { Account, Channel } from '../generated/schema'
 
 export function handleAnnouncement(event: Announcement): void {
-    log.debug(`Address of the account announcing itself: {}`, [event.params.account.toHexString()]);
+    log.info(`[ info ] Address of the account announcing itself: {}`, [event.params.account.toHexString()]);
+    let account = new Account(event.params.account.toHexString())
+    account.multiaddr = event.params.multiaddr;
+    account.channels = [];
+    account.hasAnnounced = true;
+    account.save()
 }
+
 export function handleChannelUpdate(event: ChannelUpdate): void {
-    log.debug(`Address of the account updating the channel: {}`, [event.params.source.toHexString()]);
+    log.info(`Address of the account updating the channel: {}`, [event.params.source.toHexString()]);
 }
