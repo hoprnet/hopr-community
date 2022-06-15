bd45d34f7673b9002297ca09ff85c094d0d6df27
Eliaxie <elia.maggioni@gmail.com>
2022-06-15 18:07:31 +0200

    hopr-visual

---
+++ projects/hopr-visual/src/utils/dataset-utils.ts
@@ -0,0 +1,317 @@
+import { Account, ApolloAccountQuery, ApolloChannelQuery, Channel, Dataset, RemoteStatus } from "../types";
+
+var axios = require('axios');
+
+export function datasetBuilderAccount(query: ApolloAccountQuery, dataset: Dataset): Dataset {
+    dataset.nodes = dataset.nodes.concat(...query.accounts)
+    // console.log("Dataset: ", dataset)
+    return dataset
+}
+
+export function datasetBuilderChannel(query: ApolloChannelQuery, dataset: Dataset): Dataset {
+    dataset.edges = dataset.edges.concat(...query.channels)
+    // console.log("Dataset: ", dataset)
+    return dataset
+}
+
+enum Status {
+    TypeError, NetworkError, OK, Level2Error
+}
+
+interface GetAddresses {
+    nativeAddress: string
+    hoprAddress: string
+    native: string
+    hopr: string
+}
+
+interface HoprNodeHeader {
+    peerId: string
+    multiAddr: string
+    endpoint: string
+}
+
+interface GetChannels {
+    incoming: Incoming[]
+    outgoing: Outgoing[]
+}
+
+interface Incoming {
+    type: string
+    channelId: string
+    peerId: string
+    status: string
+    balance: number
+}
+
+interface Outgoing {
+    type: string
+    channelId: string
+    peerId: string
+    status: string
+    balance: number
+}
+
+
+interface HoprNode extends HoprNodeHeader {
+    heartbeats: {
+        sent: number,
+        success: number
+    },
+    lastSeen: number,
+    quality: number,
+    backoff: number,
+    isNew: boolean
+}
+
+interface PeerRequest {
+    nodes: HoprNode[]
+    status: Status
+    error?: any
+}
+
+interface AccountRequest {
+    account: Account
+    status: Status
+    error?: any
+    node: HoprNodeHeader
+}
+
+interface ChannelRequest {
+    channel: Channel[]
+    status: Status
+    error?: any
+}
+
+export async function exploreLocalCluster(localNodeEndpoint: string, nodeToken: string, setRemoteStatus: React.Dispatch<React.SetStateAction<RemoteStatus>>, setRemoteError: React.Dispatch<React.SetStateAction<string>>): Promise<Dataset | undefined> {
+
+    let responseData: PeerRequest
+    let visitedMap: Map<string, HoprNodeHeader> = new Map()
+    let toBeVisitedList: HoprNodeHeader[] = []
+
+
+    responseData = await makePeerRequest(localNodeEndpoint, nodeToken)
+    if (responseData.status != Status.OK && responseData.nodes != undefined) {
+        setRemoteError(responseData.error)
+        setRemoteStatus(RemoteStatus.errored)
+        return
+    }
+
+    setRemoteStatus(RemoteStatus.connected)
+
+    toBeVisitedList = toBeVisitedList.concat(responseData.nodes)
+
+    setRemoteStatus(RemoteStatus.exploring)
+
+    //fetch all the nodes in the network
+    try {
+        while (toBeVisitedList.length > 0) {
+            let node: HoprNodeHeader | undefined = toBeVisitedList.shift();
+            if (node == undefined || visitedMap.has(node.peerId)) {
+                continue
+            }
+            let endpoint = parseEndpoint(node, localNodeEndpoint)
+            node.endpoint = endpoint
+            let req = await makePeerRequest(endpoint, nodeToken)
+            if (req.error != null) continue
+            visitedMap.set(node.peerId, node)
+            for (let i = 0; i < req.nodes.length; i++) {
+                const element = req.nodes[i];
+                if (element == undefined || visitedMap.has(element.peerId)) {
+                    continue
+                }
+                toBeVisitedList = toBeVisitedList.concat(element)
+            }
+
+        }
+    } catch (error) {
+        setRemoteError(responseData.error)
+        setRemoteStatus(RemoteStatus.errored)
+        return
+    }
+
+    let dataset: Dataset = {
+        nodes: [],
+        edges: []
+    }
+
+    //build nodes and edges (Accounts and Channels) for each node of the network
+    for await (const entry of visitedMap.entries()) {
+        let hoprNode = visitedMap.get(entry[0])
+        if (hoprNode == undefined) {
+            setRemoteError("[exploreLocalCluster] Hoprnode cannot be null at this point")
+            setRemoteStatus(RemoteStatus.errored)
+            return
+        }
+        let nodeRequest: AccountRequest = await makeAccount(hoprNode, nodeToken)
+        if (nodeRequest.status != Status.OK) {
+            setRemoteError(nodeRequest.error)
+            setRemoteStatus(RemoteStatus.errored)
+            return
+        }
+        let channelsRequest: ChannelRequest = await makeEdges(hoprNode, nodeToken, nodeRequest.account)
+        if (channelsRequest.status != Status.OK) {
+            setRemoteError(nodeRequest.error)
+            setRemoteStatus(RemoteStatus.errored)
+            return
+        }
+        dataset.edges = dataset.edges.concat(channelsRequest.channel)
+        dataset.nodes = dataset.nodes.concat(nodeRequest.account)
+    }
+
+    setRemoteStatus(RemoteStatus.complete)
+
+    return dataset
+}
+
+async function makePeerRequest(localNodeEndpoint: string, nodeToken: string): Promise<PeerRequest> {
+
+    let responseData: PeerRequest = {
+        nodes: [],
+        status: Status.OK
+    }
+
+    var config = {
+        method: 'get',
+        url: `https://${localNodeEndpoint}/api/v2/node/peers`,
+        //url: `https://jsonplaceholder.typicode.com/posts`,
+        headers: {
+            'accept': 'application/json',
+            'x-auth-token': nodeToken
+        },
+        timeout: 1000,
+    };
+
+    try {
+        const { data } = await axios(config)
+        try {
+            data.announced.forEach((element: HoprNode) => {
+                let hoprNode: HoprNode = {
+                    peerId: element.peerId,
+                    multiAddr: element.multiAddr,
+                    heartbeats: {
+                        sent: element.heartbeats.sent,
+                        success: element.heartbeats.success
+                    },
+                    lastSeen: element.lastSeen,
+                    quality: element.quality,
+                    backoff: element.backoff,
+                    isNew: element.isNew,
+                    endpoint: localNodeEndpoint
+                }
+                responseData.nodes = responseData.nodes.concat(hoprNode)
+            });
+        } catch (error: any) {
+            responseData.error = "Error while parsing the request. Is this a HOPR Node?"
+            responseData.status = Status.TypeError
+        }
+
+    } catch (error: any) {
+        responseData.error = error
+        responseData.status = Status.NetworkError
+    }
+
+    return responseData
+}
+
+async function makeAccount(node: HoprNodeHeader, nodeToken: string): Promise<AccountRequest> {
+
+    let account: AccountRequest = {
+        account: {
+            id: "",
+            publicKey: "",
+            balance: 0,
+            openChannelsCount: 0,
+            isActive: true
+        },
+        node: node,
+        status: Status.OK
+    }
+
+    var config = {
+        method: 'get',
+        url: `https://${node.endpoint}/api/v2/account/addresses`,
+        headers: {
+            'accept': 'application/json',
+            'x-auth-token': nodeToken
+        },
+        timeout: 1000,
+    };
+
+    let { data } = await axios(config)
+
+    try {
+        let addresses = data as GetAddresses
+        account.account.id = addresses.hoprAddress
+        account.account.publicKey = addresses.nativeAddress
+    } catch (error: any) {
+        account.error = "Level 2 error"
+        account.status = Status.Level2Error
+    }
+
+    return account
+}
+
+function parseEndpoint(node: HoprNodeHeader, localEndpoing: string): string {
+    if (localEndpoing.includes("gitpod")) {
+        let splitted = node.multiAddr.split("/")
+        let gitpodUrl = localEndpoing.substring(5)
+        let port = parseInt(splitted[4]) % 10 + 13300 //changes port 190xx to 133xx
+        return `${port}${gitpodUrl}`
+    } else {
+        let splitted = node.multiAddr.split("/")
+        let ip = splitted[2]
+        let port = parseInt(splitted[4]) % 10 + 13300 //changes port 190xx to 133xx
+        return `${ip}:${port}`
+    }
+}
+
+async function makeEdges(hoprNode: HoprNodeHeader, nodeToken: string, account: Account): Promise<ChannelRequest> {
+
+    let channelReturn: ChannelRequest = {
+        channel: [],
+        status: Status.OK
+    }
+
+    var config = {
+        method: 'get',
+        url: `https://${hoprNode.endpoint}/api/v2/channels`,
+        headers: {
+            'accept': 'application/json',
+            'x-auth-token': nodeToken
+        },
+        timeout: 1000,
+    };
+
+    let { data } = await axios(config)
+
+    try {
+        let channelsReq = data as GetChannels
+        channelsReq.outgoing.forEach((outgoing) => {
+            account.balance += outgoing.balance
+            let channel: Channel = {
+                id: outgoing.channelId,
+                source: {
+                    id: account.id
+                },
+                destination: {
+                    id: outgoing.peerId
+                },
+                balance: outgoing.balance,
+                status: 0,
+                commitment: [],
+                channelEpoch: 0,
+                ticketEpoch: 0,
+                ticketIndex: 0,
+                commitmentHistory: []
+            }
+            channelReturn.channel = channelReturn.channel.concat(channel)
+        })
+    } catch (error: any) {
+        channelReturn.error = "Level 2 error"
+        channelReturn.status = Status.Level2Error
+    }
+
+    return channelReturn
+}
+
---
+++ projects/hopr-visual/src/views/EndpointField.tsx
@@ -0,0 +1,123 @@
+import React, { KeyboardEvent, ChangeEvent, FC, useEffect, useState } from "react";
+import { useSigma } from "react-sigma-v2";
+import { Attributes } from "graphology-types";
+import { BsSearch } from "react-icons/bs";
+
+import { RemoteStatus, FiltersState, VisualMode } from "../types";
+import internal from "stream";
+import { MdPassword } from "react-icons/md";
+import { GrNodes } from "react-icons/gr";
+
+/**
+ * This component is basically a fork from React-sigma-v2's SearchControl
+ * component, to get some minor adjustments:
+ * 1. We need to hide hidden nodes from results
+ * 2. We need custom markup
+ */
+const EndpointField: FC<{ endpoint: string, remoteStatus: RemoteStatus, nodeToken: string, error: string, setNodeToken: React.Dispatch<React.SetStateAction<string>>, setRemoteEndpoint: React.Dispatch<React.SetStateAction<string>>, setRemoteStatus: React.Dispatch<React.SetStateAction<RemoteStatus>> }> = ({ endpoint, remoteStatus, nodeToken, error, setNodeToken, setRemoteEndpoint, setRemoteStatus }) => {
+  const sigma = useSigma();
+
+  // Refresh values when filters are updated (but wait a frame first):
+  useEffect(() => {
+    validateEndpoint(endpoint)
+  }, [endpoint]);
+
+  function validateEndpoint(endpoint: string): RemoteStatus {
+    const ipregex = /^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])|localhost)(:((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{2,5})|([0-9]{2,4})))$/
+    const gitpodregex = /^([0-9]{5})-hoprnet(.+)(\.gitpod\.io)$/
+    let remoteStatus = ipregex.test(endpoint) || gitpodregex.test(endpoint) ? RemoteStatus.valid : RemoteStatus.invalid
+    setRemoteStatus(remoteStatus)
+    return remoteStatus
+  }
+
+  useEffect(() => {
+  }, [remoteStatus]);
+
+  const onInputNodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setRemoteEndpoint(input);
+  };
+
+  const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && remoteStatus === RemoteStatus.valid) {
+      setRemoteStatus(RemoteStatus.selected)
+    }
+    if (e.key === "Enter" && remoteStatus !== RemoteStatus.valid) {
+      setRemoteStatus(RemoteStatus.invalid)
+      if (validateEndpoint(endpoint) === RemoteStatus.valid) {
+        setRemoteStatus(RemoteStatus.selected)
+      }
+    }
+  };
+
+  const onInputTokenChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setNodeToken(input);
+    setRemoteStatus(RemoteStatus.invalid)
+    validateEndpoint(endpoint)
+  };
+
+  function generateEndpointMessage(): React.ReactNode {
+    var classes: string[] = ["endpointInfo"]
+    var message = ""
+    switch (remoteStatus) {
+      case RemoteStatus.complete:
+        message = "Graph complete"
+        classes = classes.concat("endpointSuccess")
+        break
+      case RemoteStatus.connected:
+        message = "Connected to node"
+        classes = classes.concat("endpointSuccess")
+        break
+      case RemoteStatus.errored:
+        message = error.toString()
+        classes = classes.concat("endpointError")
+        break
+      case RemoteStatus.exploring:
+        message = "Exploring graph..."
+        break
+      case RemoteStatus.invalid:
+        message = "Endpoint is invalid"
+        classes = classes.concat("endpointError")
+        break
+      case RemoteStatus.selected:
+        message = "Connecting to node..."
+        break
+      case RemoteStatus.valid:
+        message = "Press ENTER to connect to the node"
+        classes = classes.concat("endpointSuccess")
+        break
+      default:
+    }
+    var classArray = ""
+    classes.forEach((className) => {
+      classArray += className + " "
+    })
+    return <div className={classArray}> {message} </div>
+  }
+
+  return (
+    <><div className="search-wrapper">
+      <input
+        type="search"
+        placeholder={"1.1.1.1:12345 or my.node.url"}
+        list="nodes"
+        value={endpoint}
+        onChange={onInputNodeChange}
+        onKeyPress={onKeyPress} />
+      <MdPassword className="icon" />
+    </div><div className="search-wrapper">
+        <input
+          type="search"
+          placeholder={"Insert cluster token"}
+          list="nodes"
+          value={nodeToken}
+          onChange={onInputTokenChange}
+          onKeyPress={onKeyPress} />
+        {generateEndpointMessage()}
+        <GrNodes className="icon" />
+      </div></>
+  );
+};
+
+export default EndpointField;      
